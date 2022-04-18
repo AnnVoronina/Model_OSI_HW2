27e037058813a1df516f0f0c917c06d424f83727
denispotapov <dpotapov@humans.net>
2022-04-18 22:35:58 +0300

    some refactoring

--- src/main/java/Client/Main.java
+++ src/main/java/Client/Main.java
@@ -1,56 +1,42 @@
 package Client;
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 
 public class Main {
-    private static Socket socketOfClient = null;
-    private static  PrintWriter out = null;
-    private static BufferedReader reader;
-    private static BufferedReader in = null;
     private static int port = 8099;
-    private static String outString = "";
+    private static final String serverHost = "localhost";
+    private static String outString;
 
     public static void main(String[] args) {
-        // Server Host
-        final String serverHost = "netology.homework";
-
-
-
-        try {
-            // Send a request to connect to the server is listening
-            // on machine 'localhost' port 9999.
-            socketOfClient = new Socket(serverHost, port);
-
-            //Create output stream at the client (to send data to the server)
-            // Оборачиваем в PrintWriter что бы не писать flush  в буффере
-            // true выталкивание из буфера будет происходить автоматически
-            out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socketOfClient.getOutputStream())), true);
-
-            // Input stream at Client (Receive data from the server).
-            in = new BufferedReader(new InputStreamReader(socketOfClient.getInputStream()));
-            reader = new BufferedReader(new InputStreamReader(System.in));// read from console
-            {
-            }
-        } catch (UnknownHostException e) {
-            System.err.println("Don't know about host " + serverHost);
-            return;
-        } catch (IOException e) {
-            System.err.println("Couldn't get I/O for the connection to " + serverHost);
-            return;
-        }
-        try {
-
-            while (true) {
-                outString = in.readLine();
-                if (outString.equals("end")){
-                    break;
+        try (var socketOfClient = new Socket(serverHost, port);
+             //Create output stream at the client (to send data to the server)
+             // Оборачиваем в PrintWriter что бы не писать flush  в буффере
+             // true выталкивание из буфера будет происходить автоматически
+             var out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socketOfClient.getOutputStream())), true);
+             // Input stream at Client (Receive data from the server).
+             var in = new BufferedReader(new InputStreamReader(socketOfClient.getInputStream()));
+             var reader = new BufferedReader(new InputStreamReader(System.in))// read from console
+        ) {
+            boolean isEnd = false;
+            while (!isEnd) {
+                while (in.ready()) {
+                    String s = in.readLine();
+                    if (s.equals("end")) {
+                        isEnd = true;
+                        break;
+                    }
+                    System.out.println(s);
                 }
-                System.out.println(outString);
                 String word = reader.readLine();
                 out.println(word);
-
             }
 
         } catch (UnknownHostException e) {
--- src/main/java/Server1/Main.java
+++ src/main/java/Server1/Main.java
@@ -1,8 +1,12 @@
 package Server1;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
-import java.net.Socket;
 
 
 //отослать что-то с помощью in, и принять с помощью out
@@ -10,30 +14,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ServerSocket listener = null;
-        BufferedReader in;
-        PrintWriter out;
         int port = 8099;
-        Socket socketOfServer = null;
 
-        try {
-            listener = new ServerSocket(port);
-        } catch (IOException e) {
-            System.out.println(e);
-            System.exit(1);
-        }
-
-        try {
-            System.out.println("Server is waiting to accept user...");
-            // Accept client connection request
-            // Get new Socket at Server.
-            socketOfServer = listener.accept();
-            System.out.println("Accept a client!");
-
-            // Open input and output streams
-            in = new BufferedReader(new InputStreamReader(socketOfServer.getInputStream()));
-            out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socketOfServer.getOutputStream())), true);
-
+        System.out.println("Server is waiting to accept user...");
+        try (var listener = new ServerSocket(port);
+             // Accept client connection request
+             // Get new Socket at Server.
+             var socketOfServer = listener.accept();
+             var in = new BufferedReader(new InputStreamReader(socketOfServer.getInputStream()));
+             var out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socketOfServer.getOutputStream())), true)) {
 
             while (true) {
                 out.println("Write your name!");
@@ -41,12 +30,14 @@ public class Main {
                 out.println("Are you child? (yes/no)");
                 String ageAnswer = in.readLine();
                 if (ageAnswer.equals("no") || ageAnswer.equals("yes")) {
-                    if (ageAnswer.equals("no")) {
+                    if (ageAnswer.equals("yes")) {
                         out.println("Welcome to the kids area," + name + "! Let's play!\n To end  enter any character! ");
                         out.println("end");
                         break;
-                    } else out.println("Welcome to the adult zone" + name + "! Let's drink!\n To end  enter any character!");
-                    out.println("end");
+                    } else {
+                        out.println("Welcome to the adult zone" + name + "! Let's drink!\n To end  enter any character!");
+                        out.println("end");
+                    }
                     break;
                 } else {
                     out.println("Are you child? Enter yes or no");
